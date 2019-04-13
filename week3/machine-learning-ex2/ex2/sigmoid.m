49529504513784b2234b54460522a509f377f03c
Junwon Jung <junwon3329@gmail.com>
2019-04-13 18:44:38 -0500

    Submimtted sigmoid function

--- week3/machine-learning-ex2/ex2/sigmoid.m
+++ week3/machine-learning-ex2/ex2/sigmoid.m
@@ -8,10 +8,11 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
+for i=1:size(z,1),
+  for j=1:size(z,2),
+    g(i,j) = 1 / (1 + exp(-z(i,j)));
+  endfor
+endfor
 
 % =============================================================
 
