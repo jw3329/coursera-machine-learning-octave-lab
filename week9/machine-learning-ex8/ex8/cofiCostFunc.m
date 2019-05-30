b4a7083e981fe4aade07b2d818243d896147b7eb
Junwon Jung <junwon3329@gmail.com>
2019-05-30 11:30:49 -0500

    Submitted cofiCostFunc.m file, completed

--- week9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ week9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,19 +40,15 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+for i = 1:size(X,1),
+    for j = 1:size(Theta,1),
+        if R(i,j) == 1,
+            J += (Theta(j,:)*X(i,:)' - Y(i,j))^2;
+        end
+    end
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
+J /= 2;
 
 
 % =============================================================
